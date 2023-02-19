4d0dfb93574be25ead1a79de1c33e4f3dacc4140
savfile <savfile@outlook.com>
2023-02-19 13:47:50 +0800

    feat: support RegEx and Disabled

--- csv2xml.py
+++ csv2xml.py
@@ -1,50 +1,37 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from re import search
+import csv
 
-textFile = 'blacklist.csv'
-generateFile = 'blacklist.xml'
-#generateMinFile = 'blacklist.min.xml'
+inputFile = 'blacklist.csv'
+outputFile = 'blacklist.xml'
 
-def generateXml(text):
-    with open(generateFile, 'a', encoding='utf-8') as fx:
-        if search(r'\\|\^|\||\$|\*|\+|\?|\{|\}|\.|\(|\)|\[|\]|\\s', text) != None:
-            if search(r'\(\?\<?\!', text) == None:
-                fx.write('\t<item enabled="true">r=' + text.strip('\n') + '</item>\n')
-            elif search(r'\(\?\<?\!', text) != None:
-                pass
-        #elif search(r'\\|\^|\||\$|\*|\+|\?|\{|\}|\.|\(|\)|\[|\]|\\s', text) == None:
-        else:
-            fx.write('\t<item enabled="true">t=' + text.strip('\n') + '</item>\n')
+WordLine = 0
+RegExLine = 1
+DisenabledLine = 2
 
-#def generateMinXml(text):
-    #with open(generateMinFile, 'a', encoding='utf-8') as fx:
-        #if search(r'\\|\^|\||\$|\*|\+|\?|\{|\}|\.|\(|\)|\[|\]|\\s', text) != None:
-            #if search(r'\(\?\<?\!', text) == None:
-                #fx.write('<item enabled="true">r=' + text.strip('\n') + '</item>')
-            #elif search(r'\(\?\<?\!', text) != None:
-                #pass
-        #else:
-            #fx.write('<item enabled="true">t=' + text.strip('\n') + '</item>')
 
-with open(generateFile, 'w', encoding='utf-8') as fx:
+def GenerateXml(text):
+    with open(outputFile, 'a', encoding='utf-8') as output:
+    
+        if x[RegExLine] == 'TRUE':
+            RegEx = 'r'
+        elif x[RegExLine] == '':
+            RegEx = 't'
+        
+        if x[DisenabledLine] == 'TRUE':
+            output.write('\t<item enabled="false">' + RegEx + '=' + x[WordLine] + '</item>\n')
+        elif x[DisenabledLine] == '':
+            output.write('\t<item enabled="true">' + RegEx + '=' + x[WordLine] + '</item>\n')
+
+with open(outputFile, 'w', encoding='utf-8') as fx:
     fx.write("<filters>\n")
 
-#with open(generateMinFile, 'w', encoding='utf-8') as fx:
-    #fx.write("<filters>")
+with open(inputFile, newline='', encoding='utf-8-sig') as input:
+    spamreader = csv.reader(input, delimiter=',')
+    for x in spamreader:
+        GenerateXml(x[WordLine])
 
-with open(textFile, 'r', encoding='utf-8-sig') as fp:
-    for line in fp:
-        generateXml(line)
-        #generateMinXml(line)
-    fp.close()
-
-with open(generateFile, 'a', encoding='utf-8') as fx:
+with open(outputFile, 'a', encoding='utf-8') as fx:
     fx.write('</filters>')
     fx.close()
-
-#with open(generateMinFile, 'a', encoding='utf-8') as fx:
-    #fx.write('</filters>')
-    #fx.close()
-
-print('ok')
+    
