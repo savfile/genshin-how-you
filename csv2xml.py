bf8afd9e69f94aaa3337a4d3d9d0c8ea3c51135d
savfile <savfile@outlook.com>
2023-02-25 18:37:14 +0800

    I'm so tired

--- csv2xml.py
+++ csv2xml.py
@@ -13,15 +13,16 @@ DisenabledLine = 2
 def GenerateXml(text):
     with open(outputFile, 'a', encoding='utf-8') as output:
     
-        if x[RegExLine] == 'TRUE':
-            RegEx = 'r'
-        elif x[RegExLine] == '':
-            RegEx = 't'
-        
-        if x[DisenabledLine] == 'TRUE':
-            output.write('\t<item enabled="false">' + RegEx + '=' + x[WordLine] + '</item>\n')
-        elif x[DisenabledLine] == '':
-            output.write('\t<item enabled="true">' + RegEx + '=' + x[WordLine] + '</item>\n')
+        if x[RegExLine] == 'TRUE' and x[DisenabledLine] == 'TRUE':
+            output.write('\t<item enabled="false">r=' + x[WordLine] + '</item>\n')
+        elif x[RegExLine] == '' and x[DisenabledLine] == 'TRUE':
+            output.write('\t<item enabled="false">t=' + x[WordLine] + '</item>\n')
+        elif x[RegExLine] == 'TRUE' and x[DisenabledLine] == '':
+            output.write('\t<item enabled="true">r=' + x[WordLine] + '</item>\n')
+        elif x[RegExLine] == '' and x[DisenabledLine] == '':
+            output.write('\t<item enabled="true">t=' + x[WordLine] + '</item>\n')
+        else:
+            pass
 
 with open(outputFile, 'w', encoding='utf-8') as fx:
     fx.write("<filters>\n")
@@ -32,6 +33,6 @@ with open(inputFile, newline='', encoding='utf-8-sig') as input:
         GenerateXml(x[WordLine])
 
 with open(outputFile, 'a', encoding='utf-8') as fx:
-    fx.write('</filters>')
+    fx.write('</filters>\n')
     fx.close()
     
